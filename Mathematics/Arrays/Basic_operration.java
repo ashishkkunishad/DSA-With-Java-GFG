67740d6be5f102533bc54c7a23362f545d367732
Ashish Kumar Nishad <118613984+ashishkkunishad@users.noreply.github.com>
2024-06-27 14:11:50 +0530

    update

--- Mathematics/Arrays/Basic_operration.java
+++ Mathematics/Arrays/Basic_operration.java
@@ -1,20 +1,40 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+// class gfg{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter size: ");
+//         int n=sc.nextInt();
+//         // System.out.print("Enter numbers: ");
+
+//         // int m=sc.nextInt();
+
+
+//         int[] arr=new int[n];
+
+//         for(int i=0;i<n;i++){
+//             arr[i]=sc.nextInt();
+//         }
+       
+//         for(int i=0;i<=n;i++){
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
 class gfg{
+
+ int insert(int arr[],int x,int pos){
+    if(n==)
+
+
+ }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter size: ");
-        int n=sc.nextInt();
-        // System.out.print("Enter numbers: ");
 
-        // int m=sc.nextInt();
+        int[] arr={23,445,76,5,8};
+    //    for(int i=0;i<arr.length;i++){
+    //     System.out.print(arr[i]+" ");
+    //    }
 
-
-        ArrayList<Integer> al= new ArrayList<>(n);
-
-        for(int i=0;i<=n;i++){
-            al[i]=sc.nextInt();
-        }
-        System.out.println(al);
     }
 }
